c0f962e0256870ea98ac8ce8be4a2cd0654820d6
JesReyA <jesreyav@gmail.com>
2026-04-18 15:26:13 -0600

    .

--- web_portfolio/components/galaxy.tsx
+++ web_portfolio/components/galaxy.tsx
@@ -146,7 +146,7 @@ const Home = () => {
     }, []);
 
     return (
-        <section id="home" className="relative h-screen flex items-center justify-center text-white overflow-hidden">
+        <section id="home" className="relative min-h-screen flex items-center justify-center text-white overflow-hidden">
             <GalaxyBackground />
             <canvas ref={canvasRef} className="absolute inset-0 w-full h-full">
             </canvas>
